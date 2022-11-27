f5633f5ffb584b2d5833562c4c889e57cb1917ce
Jakub Chodubski <ch.jakub23@gmail.com>
2022-11-27 15:27:40 +0100

    Added file with configuration functions

---
+++ src/helpers/configFns.js
@@ -0,0 +1,5 @@
+const setApplicationSize = () => {
+	// if (navigator.userAgent.includes("Chrome")) document.documentElement.style.setProperty("--system-height", `${window.innerHeight}px`);
+};
+
+export {setApplicationSize};
